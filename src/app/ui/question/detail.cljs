5a7b795cb2c5f25eb6aa09dd21712cc661fbdd1d
amjil <amjil@protonmail.com>
2022-07-25 09:22:28 +0800

    question detail onpress

--- src/app/ui/question/detail.cljs
+++ src/app/ui/question/detail.cljs
@@ -17,42 +17,6 @@
 
     ["react-native-vector-icons/Ionicons" :default Ionicons]))
 
-
-(def comments [{:user_name "john" :content "hello"}
-               {:user_name "sara" :content "hello, John"}
-               {:user_name "peter" :content "what a nice day"}
-               {:user_name "Lily" :content "so, what is the best there?"}])
-
-(defn comment-view [h item]
-  [nbase/vstack {:flex 1 :ml 2 :mt 1}
-   [nbase/box {:justifyContent "flex-start" :alignItems "flex-start"}
-    [nbase/box {:bg (theme/color "gray.300" "gray.500") :borderRadius "md" :p 6}]]
-   [nbase/hstack {:flex 1 :mt 2}
-    [nbase/vstack {:mr 2}
-     [nbase/box  {:mb 2 :justifyContent "center" :alignItems "center"}
-      [text/measured-text {:fontSize 18 :color (theme/color "#71717a" "#9ca3af") :width (- @h 48)} (item :user_name)]]
-     [nbase/box  {:justifyContent "center" :alignItems "center"}
-      [text/measured-text {:fontSize 10 :color "#a1a1aa"} "09:15"]]]
-    [text/measured-text {:fontSize 18 :color (theme/color "#71717a" "#9ca3af") :width (- @h 48)} (item :content)]]])
-
-(defn answer-buttons [h model]
-  [srn/view {:style {:height @h :margin-top 5 :margin-horizontal 20}}
-   ; [srn/view {:style {:margin-bottom 20 :borderRadius 10 :backgroundColor "#eff6ff" :justifyContent "center" :alignItems "center"}}]
-   [nbase/vstack {:mb 4 :borderRadius "full" :bg (theme/color "blue.50" "dark.300") :justifyContent "center" :alignItems "center"}
-    [nbase/icon-button {:justifyContent "center" :alignItems "center"
-                        :icon (reagent/as-element [nbase/icon {:as Ionicons :name "caret-up-outline" :size "5" :color (theme/color "blue.600" "blue.800")}])}]
-    [text/measured-text {:fontSize 12 :color (theme/color "#2563eb" "#1e40af")} (str (get-in labels [:question :vote]) "  " (:agree_count model))]
-    [nbase/icon-button {:justifyContent "center" :alignItems "center"
-                        :icon (reagent/as-element [nbase/icon {:as Ionicons :name "caret-down-outline" :size "5" :color (theme/color "blue.600" "blue.800")}])}]]
-   [nbase/vstack {:mt 2 :borderRadius "full" :bg (theme/color "blue.50" "dark.300") :justifyContent "center" :alignItems "center"}
-   ; [srn/view {:style {:margin-top 10 :borderRadius 10 :backgroundColor "#eff6ff" :justifyContent "center" :alignItems "center"}}
-    [nbase/icon-button {:mb 4 :justifyContent "center" :alignItems "center"
-                        :icon (reagent/as-element [nbase/icon {:as Ionicons :name "heart-outline"  :size "5" :color (theme/color "blue.600" "blue.800")}])}]
-    [nbase/icon-button {:mb 4 :justifyContent "center" :alignItems "center"
-                        :icon (reagent/as-element [nbase/icon {:as Ionicons :name "star-outline" :size "5" :color (theme/color "blue.600" "blue.800")}])}]
-    [nbase/icon-button {:justifyContent "center" :alignItems "center"
-                        :icon (reagent/as-element [nbase/icon {:as Ionicons :name "chatbubble-outline" :size "5" :color (theme/color "blue.600" "blue.800")}])}]]])
-
 (defn detail-view []
   (let [h (reagent/atom 0)
         modal-open (reagent/atom false)
@@ -90,37 +54,41 @@
              (fn [x]
                (let [{:keys [item index separators]} (j/lookup x)]
                  (reagent/as-element
-                   [nbase/flex {:m 1 :flex-direction "row" :bg (theme/color "white" "dark.100")}
-                    [nbase/vstack
-                     [nbase/box {:bg (theme/color "gray.300" "gray.500")
-                                 :borderRadius "md"
-                                 :p 4
-                                 :alignSelf "center"}]
-                     [nbase/box {:alignSelf "center"
-                                 :justifyContent "center"
-                                 :mt 4}
-                      [nbase/hstack {:bg (theme/color "white" "dark.100")}
-                       [text/measured-text {:fontSize 18 :color "#71717a" :width (- @h 48) } (j/get item :user_name)]
-                       [text/measured-text {:fontSize 10 :color "#a1a1aa"} "09:15"]]]]
-                    [nbase/box {:m 1 :ml 2 :mt 12
-                                :bg (theme/color "white" "dark.100")}
-                     [text/measured-text {:fontSize 18 :color "#71717a" :width (- @h 48)} (j/get item :content)]]
-                    [nbase/vstack {:m 1 :mt 12 :ml 2
-                                   :justifyContent "space-between"
-                                   :bg (theme/color "white" "dark.100")}
-                     [nbase/icon {:as Ionicons :name "ios-ellipsis-vertical-sharp"
-                                  :size "4" :color "indigo.500"}]
+                   [nbase/pressable {:on-press
+                                     (fn [e]
+                                       (re-frame/dispatch [:navigate-to :question-detail2])
+                                       (re-frame/dispatch [:get-answer (j/get item :question_id) (j/get item :id)])
+                                       (re-frame/dispatch [:answer-comments (j/get item :id)]))}
+                    [nbase/flex {:m 1 :flex-direction "row" :bg (theme/color "white" "dark.100")}
+                     [nbase/vstack
+                      [nbase/box {:bg (theme/color "gray.300" "gray.500")
+                                  :borderRadius "md"
+                                  :p 4
+                                  :alignSelf "center"}]
+                      [nbase/box {:alignSelf "center"
+                                  :justifyContent "center"
+                                  :mt 4}
+                       [nbase/hstack {:bg (theme/color "white" "dark.100")}
+                        [text/measured-text {:fontSize 18 :color "#71717a" :width (- @h 48) } (j/get item :user_name)]
+                        [text/measured-text {:fontSize 10 :color "#a1a1aa"} "09:15"]]]]
+                     [nbase/box {:m 1 :ml 2 :mt 12
+                                 :bg (theme/color "white" "dark.100")}
+                      [text/measured-text {:fontSize 18 :color "#71717a" :width (- @h 48)} (j/get item :content)]]
+                     [nbase/vstack {:m 1 :mt 12 :ml 2
+                                    :bg (theme/color "white" "dark.100")}
+                      [nbase/icon {:as Ionicons :name "ios-ellipsis-vertical-sharp"
+                                   :size "4" :color "indigo.500" :mb 6}]
 
-                     [nbase/vstack {:mb 8}
-                      [nbase/box {:mb 6 :alignItems "center"}
-                       [nbase/icon {:as Ionicons :name "heart-outline"
-                                    :size "4" :color "indigo.500"}]
-                       [text/measured-text {:color "#d4d4d8"} "1024"]]
-                      [nbase/box {:mb 6 :alignItems "center"}
-                       [nbase/icon {:as Ionicons :name "chatbox-outline"
-                                    :size "4" :color "indigo.500"}]
-                       [text/measured-text {:color "#d4d4d8"} "128"]]
-                      [text/measured-text {:color "#525252"} (time/month-date-from-string (j/get item :created_at))]]]])))}]]]]))))
+                      [nbase/vstack {:mb 8}
+                       [nbase/box {:mb 6 :alignItems "center"}
+                        [nbase/icon {:as Ionicons :name "heart-outline"
+                                     :size "4" :color "indigo.500"}]
+                        [text/measured-text {:color "#d4d4d8"} "1024"]]
+                       [nbase/box {:mb 6 :alignItems "center"}
+                        [nbase/icon {:as Ionicons :name "chatbox-outline"
+                                     :size "4" :color "indigo.500"}]
+                        [text/measured-text {:color "#d4d4d8"} "128"]]
+                       [text/measured-text {:color "#525252"} (time/month-date-from-string (j/get item :created_at))]]]]])))}]]]]))))
 
 
 
