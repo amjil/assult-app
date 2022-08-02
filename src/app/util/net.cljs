301fcc8b0acec865bb8fb2610016d57f2eb305f2
amjil <amjil@protonmail.com>
2022-08-02 22:17:15 +0800

    net

--- src/app/util/net.cljs
+++ src/app/util/net.cljs
@@ -3,7 +3,34 @@
    [re-frame.core :as re-frame]
    [applied-science.js-interop :as j]
    [cljs-bean.core :as bean]
+   [promesa.core :as p]
+
+   [cljs.core.async :refer [go]]
+   [cljs.core.async.interop :refer-macros [<p!]]
+
    ["@react-native-community/netinfo" :default net-info]))
+
+(defn configure []
+  (j/call
+   net-info
+   :configure
+   (bean/->js
+    {:reachabilityUrl "http://t.amjil.net/api/ping"
+     :reachabilityTest
+     #(go (= (j/get % :status) 200))
+     :reachabilityLongTimeout (* 60 1000)
+     :reachabilityShortTimeout (* 5 1000)
+     :reachabilityRequestTimeout (* 15 1000)
+     :reachabilityShouldRun (fn [] true)
+     :shouldFetchWiFiSSID true
+     :useNativeReachability false})))
+
+(defn net-info-fetch []
+  (-> (j/call net-info :fetch)
+      (p/then
+       (fn [state]
+         (js/console.log "Connection Type" (j/get state :type))
+         (js/console.log "is connected? " (j/get state :isConnected))))))
 
 ;; copied from status-im source code
 ;; (defn add-net-info-listener []
@@ -28,5 +55,7 @@
    (fn [state]
      (js/console.log "Connection Type" (j/get state :type))
      (js/console.log "is connected? " (j/get state :isConnected))))
+  (configure)
+  (net-info-fetch)
   (prn "hi")
   )
